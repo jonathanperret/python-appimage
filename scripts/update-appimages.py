41fbbb636b953c6f134486bd4e72895ed72c3b39
Jonathan Perret <j-github@jonathanperret.net>
2025-04-16 14:30:16 +0200

    3.11 only

--- scripts/update-appimages.py
+++ scripts/update-appimages.py
@@ -72,21 +72,21 @@ class AssetMeta:
 
     def appimage_name(self):
         '''Returns Python AppImage name'''
-        return format_appimage_name(self.abi, self.version, self.tag)
+        return format_appimage_name(self.abi, self., self.tag)
 
     def formated_tag(self):
         '''Returns formated manylinux tag'''
         return format_tag(self.tag)
 
-    def previous_version(self):
-        '''Returns previous version'''
+    def previous_(self):
+        '''Returns previous '''
         if self.asset:
             return self.asset.name[6:-9].split('-', 1)[0]
 
     def release_tag(self):
         '''Returns release git tag'''
-        version = self.version.rsplit('.', 1)[0]
-        return f'python{version}'
+         = self..rsplit('.', 1)[0]
+        return f'python{}'
 
 
 def update(args):
@@ -130,7 +130,7 @@ def update(args):
     assets = defaultdict(dict)
     n_assets = 0
     for release in repo.get_releases():
-        if release.tag_name.startswith('python'):
+        if release.tag_name == 'python3.11':
             meta = ReleaseMeta(
                 tag = release.tag_name,
                 release = release
@@ -162,7 +162,7 @@ def update(args):
                 continue
 
             pythons = list_pythons(tag)
-            for (abi, version) in pythons:
+            for (abi, ) in pythons:
                 try:
                     meta = assets[tag][abi]
                 except KeyError:
